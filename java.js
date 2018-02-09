a09460f09ebc50a241b97378f3a1c218c116e2b0
Sahsa29 <obutenko318@mail.com>
2018-02-09 13:00:04 +0200

    first_commit

---
+++ java.js
@@ -0,0 +1,19 @@
+
+var i = 0;
+ while(i<100){
+ 	if(i%3==0)
+ 	if(i%3==0 && i%2!==0){
+ 		console.log(i);
+ 	}
+ 	i++;
+ }
+
+
+
+var a = (9);
+fact = 1;
+while(a>0){
+	fact *= a;
+	a--;
+}
+ document.write(a);
